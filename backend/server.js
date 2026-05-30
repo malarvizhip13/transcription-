3f789e095d2929c6d6216efb3109153d14430d80
malarvizhi <malarvizhi.p8248@gmail.com>
2026-05-30 16:40:48 +0530

    day 4

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
+
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-
+const multer = require("multer");
+const fs = require("fs");
+const OpenAI = require("openai");
 const app = express();
 
 app.use(cors());
@@ -13,6 +17,21 @@ app.get("/", (req, res) => {
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage });
 
 
 
