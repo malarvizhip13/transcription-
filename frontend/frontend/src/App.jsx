e34286f1efc75b844906d844ee93920ef30273da
malarvizhi <malarvizhi.p8248@gmail.com>
2026-05-30 16:12:37 +0530

    day 1

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold  ">
+        Speech To Text App
+      </h1>
+    </div>
+  
+export default App;
+    </>
+  )
+}
+
+export default App
